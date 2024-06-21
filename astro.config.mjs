0ac60755468876198dfef585edf0be7fd98d3d9f
Dhanush Suvarna <dhanush@tenovia.com>
2024-06-21 10:34:59 +0530

    add base on astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tensight-hub.github.io/',
+	site: 'https://tensight-hub.github.io',
+	base: 'technical-docs',
 	integrations: [mdx(), sitemap()],
 });
